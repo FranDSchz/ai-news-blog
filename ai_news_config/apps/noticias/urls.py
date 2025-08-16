86157e68823d2b64658026d008be646ecda4a02b
FranDSchz <francodamiansanchez10@gmail.com>
2025-08-15 22:09:57 -0300

    add templates home, base

--- ai_news_config/apps/noticias/urls.py
+++ ai_news_config/apps/noticias/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import home, post_detail
 
 urlpatterns = [
-    path('<int:id_post>/', post_detail, name="post_detail"),
+    path('<int:pk>/', post_detail, name="post_detail"),
 ]
--- ai_news_config/apps/noticias/views.py
+++ ai_news_config/apps/noticias/views.py
@@ -5,10 +5,10 @@ from .models import Post
 def home(request):
     posts = Post.objects.filter(estado='publicado').order_by('-fecha_publicacion')
     context = {'posts':posts}
-    return render(request,'home.html',context)
+    return render(request,'noticias/home.html',context)
 
-def post_detail(request,id_post):
-    post = get_object_or_404(Post,id=id_post)
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
     context = {'post':post}
     print(post)
     return render(request,'noticias/post_detail.html',context)
