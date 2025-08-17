12823b8337adefd46b4f907df67d4b36f24f4eef
FranDSchz <francodamiansanchez10@gmail.com>
2025-08-17 02:27:01 -0300

    Feature: recupero y aplico el trabajo del home dinámico

--- ai_news_config/apps/noticias/views.py
+++ ai_news_config/apps/noticias/views.py
@@ -1,35 +1,42 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
-from .models import Post
+from .models import Post, Categoria
 from .forms import PostForm 
 
 #DEUDA TECNICA: Implementar manejo de errores, que pasa si hay menos de 10 noticias publicadas?
 def home(request):
-    # 1. Hacemos una consulta para obtener los 10 posts más recientes.
-    #    Pedimos 10 para cubrir todas las secciones (1 top + 3 bottom + 6 right).
-    posts_recientes = Post.objects.filter(estado='publicado').order_by('-fecha_publicacion')[:10]
+    posts_recientes = Post.objects.filter(estado='publicado').order_by('-fecha_publicacion')[:25]
     
+    categorias_whats_new = Categoria.objects.all()
+    categorias_con_posts = []
+    for cat in categorias_whats_new:
+        aux = Post.objects.filter(categoria=cat).order_by('-fecha_publicacion')[:4]
+        categorias_con_posts.append({
+            'cat': cat,
+            'posts': aux
+        })
+        
+    posts_whats_new = posts_recientes[:4]
     #CUANDO QUIERA APLICAR OTRO CRITERIO VOY A TENER QUE CAMBIAR COMO DEFINO ESTAS VARIABLES
     trending_title_posts = posts_recientes[:5]
     trending_top_post = posts_recientes[0] if len(posts_recientes) > 0 else None
     trending_bottom_posts =  posts_recientes[1:4]
     right_content_posts = posts_recientes[4:9]
-    # 2. Preparamos el contexto con nombres claros para cada sección.
-    #    Usamos slicing para dividir la lista de posts.
+    weekly_news_posts = posts_recientes[10:14]
+    weekly2_news_posts= posts_recientes[14:19]
+    recent_articles_posts = posts_recientes[19:23]
     context = {
-        # Para el "Trending Title", podemos reusar los 5 primeros.
         'trending_title_posts': trending_title_posts ,
-        
-        # El post principal es el primero de la lista (índice 0).
-        # Agregamos una comprobación para evitar errores si no hay posts.
         'trending_top_post': trending_top_post,
-        
-        # Los 3 siguientes son para la sección de abajo (del índice 1 al 4, sin incluir el 4).
         'trending_bottom_posts': trending_bottom_posts,
-
-        # Los 6 restantes son para la barra lateral derecha (del índice 4 al 10). 
-        'right_content_posts': right_content_posts
+        'right_content_posts': right_content_posts,
+        'weekly_news_posts': weekly_news_posts,
+        'categorias_whats_new': categorias_whats_new,
+        'posts_whats_new': posts_whats_new,
+        'weekly2_news_posts': weekly2_news_posts,
+        'recent_articles_posts': recent_articles_posts,
+        'data': categorias_con_posts
     }
     
     return render(request, 'noticias/home.html', context)
